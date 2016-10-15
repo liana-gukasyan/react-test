1ce168805ae956bf4d2b28988d9a139feb68c033
Komp <lia.gukasyan@gmail.com>
2016-10-15 17:34:56 +0300

    Make sketch todoapp

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 
-class HelloWorld extends React.Component {
+let tasks = [
+	{id: '1', text: 'Make some coding', completed: true},
+  {id: '2', text: 'Go to bed', completed: false}
+]
+
+function findIndex(id) {
+  var tempInd = -1
+  tasks.forEach(function(item, i, tasks) {
+    if (item.id == id) {
+      tempInd = i
+    }
+  })
+  return tempInd
+}
+
+function onTaskEdit(id, newText) {
+	let taskIndex = findIndex(id)
+	let currentTask = tasks[taskIndex]
+  currentTask.text = newText
+	renderAll()
+}
+
+class TodoApp extends React.Component {
+
+	renderTasks() {
+		let tasks = this.props.tasks.map((task) => {
+			let onTaskEdit = (e) => {
+				this.props.onTaskEdit(task.id, 'ururu')
+			}
+			return (
+				<div key={task.id} onDoubleClick={onTaskEdit}>
+					{task.text}
+				</div>
+			)
+		})
+		return tasks
+	}
+
   render() {
+  	let tasks = this.renderTasks()
     return (
-      <div>Hello World</div>
-    );
+      <div>
+      	{tasks}
+      </div>
+    )
   }
 }
 
-render(<HelloWorld />, document.getElementById('container'));
+function renderAll() {
+	render(
+		<TodoApp tasks={tasks}
+						 onTaskEdit={onTaskEdit}/>,
+		document.getElementById('container'))
+}
+
+renderAll()
